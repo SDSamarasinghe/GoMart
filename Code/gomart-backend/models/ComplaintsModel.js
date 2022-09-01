66bb7b996a33243d06915e4274d461bc9dd20030
RandikaBatawala <randika.batawala99@gmail.com>
2022-09-02 00:33:22 +0530

    fixed some bugs

--- Code/gomart-backend/models/ComplaintsModel.js
+++ Code/gomart-backend/models/ComplaintsModel.js
@@ -3,27 +3,22 @@ const mongoose = require('mongoose');
 const complaintsSchema = new mongoose.Schema({
     fname: {
         type: String,
-        required: true
+       
     },
     lname: {
         type: String,
-        required: true
     },
     date: {
         type: String,
-        required: true
     },
     email: {
         type: String,
-        required: true
     },
     category: {
         type: String,
-        required: false
     },
     Issue:{
         type: String,
-        required: false
     },
     files:{
         type: String,
--- Code/gomart-backend/models/FeedbackModel.js
+++ Code/gomart-backend/models/FeedbackModel.js
@@ -3,39 +3,39 @@ const mongoose = require('mongoose');
 const feedbacksSchema = new mongoose.Schema({
     fname: {
         type: String,
-        required: true
+        
     },
     lname: {
         type: String,
-        required: true
+        
     },
     date: {
         type: String,
-        required: true
+       
     },
     email: {
         type: String,
-        required: true
+        
     },
     pro_qua_fb: {
         type: String,
-        required: false
+        
     },
     deli_dri_rate: {
         type: String,
-        required: false
+        
     },
     deli_tme_rate:{
         type: String,
-        required: false
+       
     },
     recommendation:{
         type: String,
-        required: true
+       
     },
     suggestions:{
         type: String,
-        required: true
+        
     }
 
 
