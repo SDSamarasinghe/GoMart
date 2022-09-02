5968ee8f5e6efea542bd44fd85dc4915d50a5bd7
SDSamarasinghe <it20605902@my.sliit.lk>
2022-09-02 20:27:18 +0530

    cache commit

--- Code/gomart-backend/server.js
+++ Code/gomart-backend/server.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/Products", require("./routes/productRoutes"));
 app.use("/api/Feedbacks", require("./routes/feedbackRoutes"));
-app.use("/api/Complaints", require("./routes/complaintRoutes"));
+app.use("/api/Complaints", require("./routes/complaintsRoutes"));
 app.use("/api/order", require("./routes/orderRoutes"));
 
 
