35b674439b4c5f9f603fe70991c37e8ef039675a
rAVINDUpRABASHWARA <00prabashwara123@gmail.com>
2022-10-15 12:44:54 +0530

    Update the register user function and login. Also push the user details to the localstorage

--- Code/gomart-backend/controllers/UserManagerControllers.js
+++ Code/gomart-backend/controllers/UserManagerControllers.js
@@ -1,38 +1,42 @@
 const AppUser = require("../models/UserModel");
 
 const registerUser = (req, res) => {
-  let { full_name, email, password } = req.body;
+  const {
+    full_name,
+    email,
+    password
+} = req.body;
 
-  AppUser.create({ full_name, email, password }, (err, data) => {
-    if (err) res.status(500).json({ error: err });
-    res.status(201).json(data);
-  });
+const newUser = new AppUser({
+    full_name,
+    email,
+    password
+});
+
+newUser.save()
+    .then((customer) => {
+        res.status(200).send({status: "Customer Registered", customer});
+    }).catch((err) => {
+        console.log(err);
+    })
 };
 
-const loginUser = (req, res) => {
-  console.log(req.body);
+const loginUser = async(req, res) => {
+  let UserName = req.body.email;
+    let Password = req.body.password;
 
-  if (
-    !req.body.email ||
-    !req.body.password ||
-    req.body.password === null ||
-    req.body.email === null
-  ) {
-    res.status(401).json({ error: "Email or Password doesn't match" });
-  }
+    await AppUser.findOne({email: UserName}).then((data) => {
+        if(data){
+            if(data.password == Password){
+                res.status(200).send({status: "Login Successfully", data});
+            }else{
+                res.send({status: "Incorrect Password"});
+            }
+        }else{
+            res.send({status: "User does not exists"});
+        }
+    });
 
-  AppUser.findOne({ email: req.body.email }, function (err, doc) {
-    if (err) {
-      res.status(401).json({ error: "Email or Password doesn't match" });
-    } else {
-      if (req.body.password === doc.password) {
-        req.session.user = doc;
-        res.status(200).json(doc);
-      } else {
-        res.status(401).json({ error: "Password doesn't match" });
-      }
-    }
-  });
 };
 
 const currentUser = (req, res) => {
@@ -67,7 +71,7 @@ const updateUser = async (req, res) => {
       Address
   }
 
-  const update = await User.findByIdAndUpdate(UserId, updateUser)
+  const update = await AppUser.findByIdAndUpdate(UserId, updateUser)
       .then((update) => {
           res.status(200).send({status: "Customer Updated", update});
       }).catch((err) => {
